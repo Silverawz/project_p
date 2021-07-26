a77b9c558d83242f1f5d66c156df3a4fe67300b6
silverawz <deroussenicolas@gmail.com>
2021-07-26 14:55:09 +0200

    working on animation

--- script.js
+++ script.js
@@ -1,23 +1,48 @@
 
 
-           window.onload = function initialize(){
-               console.log("init done");
-                this.burger();
-            }
+
+
+
+window.onload = function initialize(){
+    console.log("init done");
+    this.burger();
+    if(window.innerWidth > 800) {
+        this.animateFirstSection(true);
+    } else{
+        this.animateFirstSection(false);
+    }
+}
            
-            function burger(){
-                const hamburger = document.querySelector(".hamburger");
-                const navLinks = document.querySelector(".nav-links");
-                const links = document.querySelectorAll(".nav-links li");
-                hamburger.addEventListener('click', ()=>{
-                   //Animate Links
-                   
-                    navLinks.classList.toggle("open");
-                    links.forEach(link => {
-                        link.classList.toggle("fade");
-                    });
+function burger(){
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+    const links = document.querySelectorAll(".nav-links li");
+
+    hamburger.addEventListener('click', ()=>{
+        //Animate Links
+        navLinks.classList.toggle("open");  
+        links.forEach(link => {
+            link.classList.toggle("fade");
+        });
                 
-                    //Hamburger Animation
-                    hamburger.classList.toggle("toggle");
-                });
-              }
+        //Hamburger Animation
+        hamburger.classList.toggle("toggle");
+    });
+}
+
+function animateFirstSection(notMobileUser){
+    const un = document.querySelector(".un");
+    const deux = document.querySelector(".deux");
+    const trois = document.querySelector(".trois");
+    const quatre = document.querySelector(".quatre");
+    if(notMobileUser){
+        un.classList.add("animate");
+        deux.classList.add("animate");
+        trois.classList.add("animate");
+        quatre.classList.add("animate");
+    } else{
+
+    }
+
+
+}
